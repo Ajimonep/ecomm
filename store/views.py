0b59846bc76e9f66b9939e94623539b27c1bab40
Ajimon <ajimon031@gmail.com>
2024-11-20 08:42:39 +0530

    message

--- store/views.py
+++ store/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from django.views.generic import View
 
 from store.forms import SignUpForm
+
+from django.core.mail import send_mail
+
+def send_otp_email(user):
+
+    user.generate_otp()
+
+    subject="verify your email"
+
+    message=f"otp for account verification is {user.otp}"
+
+    from_email="ajimon031@gmail.com"
+
+    to_email=[user.email]
+
+    send_mail(subject,message,from_email,to_email )
+
 
 
 class SignUpView(View):
@@ -16,3 +33,25 @@ class SignUpView(View):
         form_instance=self.form_class()
 
         return render(request,self.template_name,{"form":form_instance})
+
+    def post(self,request,*args,**kwargs):
+
+        form_data=request.POST 
+
+        form_instance=self.form_class(form_data)
+
+        if form_instance.is_valid():
+
+            user_object=form_instance.save(commit=False)
+
+            user_object.is_active=False
+
+            user_object.save()
+
+            send_otp_email(user_object)
+
+            # return return(request,)
+
+            pass
+
+    
